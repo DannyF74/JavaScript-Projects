9e115fbd5af65e146834b848dfeb6bcdadfafdc0
DannyF74 <Dannytfirth@outlook.com>
2024-05-07 14:52:37 -0700

    Added comments to the JS code

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,12 +1,16 @@
-//var B1="You have clicked button 1!"
+//Testing out the difference and the priority between global and local variables
 B2="You have clicked button 2!";
 
+
+//Function 1 declares a variable and an addition to the variable. This will find the element with the output ID and replace the inner HTML
+//of the element in the HTML code with this local variable.
 function function_1() {
     var B1="You have clicked button 1!";
     B1+=" Congrats!";
     document.getElementById("output").innerHTML = B1;
 }
 
+//Function 2 will do the same as function 1 but it will use a global variable rather than a local one.
 function function_2() {
     document.getElementById("output").innerHTML = B2;
 }
