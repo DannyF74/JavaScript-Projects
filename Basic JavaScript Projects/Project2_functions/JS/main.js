171428f2e8a778512954a673cd82cf3449afb90d
DannyF74 <Dannytfirth@outlook.com>
2024-05-07 14:46:38 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+//var B1="You have clicked button 1!"
+B2="You have clicked button 2!";
+
+function function_1() {
+    var B1="You have clicked button 1!";
+    B1+=" Congrats!";
+    document.getElementById("output").innerHTML = B1;
+}
+
+function function_2() {
+    document.getElementById("output").innerHTML = B2;
+}
+
