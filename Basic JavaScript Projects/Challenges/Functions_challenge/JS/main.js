7530eca14d34e59ca68f2e9b41b653dd3b8f68ac
DannyF74 <Dannytfirth@outlook.com>
2024-05-08 14:26:59 -0700

    Create main.js

---
+++ Basic JavaScript Projects/Challenges/Functions_challenge/JS/main.js
@@ -0,0 +1,27 @@
+//The convert celcius_to_fahrenheit function
+//var temp = document.getElementById("fahrenheit");
+
+//let temp = convert();
+
+//function convert(temp) {
+//    (5/9) * (temp-32);
+//}
+
+//function display_celcius(){
+//    convert();
+//    document.getElementById("celcius").innerHTML=temp;
+//    return;
+//}
+
+//Was messing around with a celcius to fahrenheit converter function for a while but couldn't get it working just yet. I imagine this will be covered later in the course.
+//In the mean time, I will just make a basic function below.
+
+let x = myFunction(2, 2);
+document.getElementById("answer").innerHTML = x;
+
+function myFunction(a, b) {
+    return a + b;
+}
+
+
+
