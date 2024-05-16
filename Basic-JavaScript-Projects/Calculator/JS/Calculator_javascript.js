b654e8bf767b70295872b52a0ca12275ba4a95a2
DannyF74 <Dannytfirth@outlook.com>
2024-05-16 12:08:23 -0700

    Update Calculator_javascript.js

--- Basic-JavaScript-Projects/Calculator/JS/Calculator_javascript.js
+++ Basic-JavaScript-Projects/Calculator/JS/Calculator_javascript.js
@@ -1,7 +1,7 @@
 //Creates an object to keep track of values.
 const calculator = {
     //This will display 0 on the calculator screen.
-    display_value: 0,
+    Display_Value: '0',
     //This will hold the first operand for any expressions, we will set it to null for now.
     first_operand: null,
     //this checks whether or not the second operand has been inputted by the user.
@@ -11,17 +11,17 @@ const calculator = {
 };
 
 //This modifies values each time a button is clicked on.
-function input_digit(digit) {
-    const { display_value, wait_second_operand } = calculator;
+function Input_Digit(digit) {
+    const {Display_Value, wait_second_operand } = calculator;
     //This checks if the wait_second_operand is true and sets display value
     //to the key that was clicked on.
     if (wait_second_operand === true) {
-        calculator.display_value = digit;
+        calculator.Display_Value = digit;
         calculator.wait_second_operand = false;
     } else {
-        //This overwrites display_value if the current value is 0
+        //This overwrites Display_Value if the current value is 0
         //otherwise it adds on to it.
-        calculator.display_value = display_value === 0 ? digit : display_value + digit;
+        calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
     }
 }
 
@@ -30,20 +30,20 @@ function input_decimal(dot) {
     //This ensures that accidental clicking of the decimal point doesn't
     //cause bugs in the operation.
     if (calculator.wait_second_operand === true) return;
-    if (!calculator.display_value.includes(dot)) {
-        //We are saying that if the display_value does not contain a decimal point
+    if (!calculator.Display_Value.includes(dot)) {
+        //We are saying that if the Display_Value does not contain a decimal point
         //we want to add a decimal point.
-        calculator.display_value += dot;
+        calculator.Display_Value += dot;
     }
 }
 
 //This section handles operators
 function handle_operator(next_operator) {
-    const{first_operand, display_value, operator} = calculator;
+    const{first_operand, Display_Value, operator} = calculator;
     //When an operator key is pressed we convert the current number
     //displayed on the screen to a numver and then store the result in
     //calculator.first_operand if it doesn't already exist
-    const value_of_input = parseFloat(display_value);
+    const value_of_input = parseFloat(Display_Value);
     //Checks if an operator already exists and if wait_second_operand is true,
     //then updates the operator and exits from the function.
     if (operator && calculator.wait_second_operand) {
@@ -62,7 +62,7 @@ function handle_operator(next_operator) {
         result= Number(result).toFixed (9);
         //this will remove any trailing 0's
         result = (result *1).toString();
-        calculator.display_value = parseFloat(result);
+        calculator.Display_Value = parseFloat(result);
         calculator.first_operand = parseFloat(result);
     }
     calculator.wait_second_operand = true;
@@ -76,17 +76,17 @@ const perform_calculation = {
     '=' : (first_operand, second_operand) => second_operand,
 };
 function calculator_reset() {
-    calculator.display_value = '0';
+    calculator.Display_Value = '0';
     calculator.first_operand = null;
     calculator.wait_second_operand = false;
     calculator.operator = null;
 }
-//This function updates the calculator screen with the contents of display_value
+//This function updates the calculator screen with the contents of Display_Value
 function update_display() {
     //Makes use of the calculator_screen class to target the
     //input tag in the HTML document
     const display = document.querySelector('.calculator_screen');
-    display_value = calculator.display_value;
+    display.value = calculator.Display_Value;
 }
 
 update_display();
@@ -116,6 +116,7 @@ keys.addEventListener('click', (event) => {
         update_display();
         return;
     }
-    input_digit(target.value);
+    Input_Digit(target.value);
     update_display();
 })
+
